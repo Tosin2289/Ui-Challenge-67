5d2a86dd0d5f7186cfb89fe3953858fc7a05f65f
Tosin2289 <ayoolatosin00@gmail.com>
2023-03-10 21:25:51 +0100

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,289 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: SafeArea(child: MyApp()),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final fntfamily = GoogleFonts.nunito().fontFamily;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    Widget imgprovider({required String img}) {
+      return Container(
+        height: screenHeight * 0.09,
+        width: screenWidth * 0.78 / 4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          image: DecorationImage(image: AssetImage(img), fit: BoxFit.cover),
+        ),
+      );
+    }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            SizedBox(height: 24),
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 24, right: 24),
+                  child: Center(
+                    child: Container(
+                      height: screenHeight * 0.42,
+                      width: screenWidth,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/img1.png"),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.circular(28),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 25,
+                  left: 46,
+                  child: Container(
+                    height: 70,
+                    width: 185,
+                    decoration: BoxDecoration(
+                      color: Colors.white24,
+                      borderRadius: BorderRadius.circular(28),
+                    ),
+                    child: Row(
+                      children: [
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 12),
+                              child: CircleAvatar(
+                                child: CircleAvatar(
+                                  radius: 20,
+                                  backgroundColor: Colors.grey,
+                                  backgroundImage:
+                                      AssetImage("assets/img2.png"),
+                                ),
+                                backgroundColor: Colors.white,
+                                radius: 22,
+                              ),
+                            ),
+                            Positioned(
+                              left: 40,
+                              child: CircleAvatar(
+                                child: CircleAvatar(
+                                  radius: 20,
+                                  backgroundColor: Colors.grey,
+                                  backgroundImage:
+                                      AssetImage("assets/img3.jpg"),
+                                ),
+                                backgroundColor: Colors.white,
+                                radius: 22,
+                              ),
+                            ),
+                            Positioned(
+                              left: 70,
+                              child: CircleAvatar(
+                                child: CircleAvatar(
+                                  radius: 20,
+                                  backgroundColor: Colors.grey,
+                                  backgroundImage:
+                                      AssetImage("assets/img4.jpg"),
+                                ),
+                                backgroundColor: Colors.white,
+                                radius: 22,
+                              ),
+                            ),
+                            Positioned(
+                              left: 100,
+                              child: CircleAvatar(
+                                child: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    foregroundColor: Colors.tealAccent.shade400,
+                                    radius: 20,
+                                    child: (Text(
+                                      "+5",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: fntfamily),
+                                    ))),
+                                backgroundColor: Colors.white,
+                                radius: 22,
+                              ),
+                            ),
+                            Positioned(
+                              left: 150,
+                              top: 10,
+                              child: Center(
+                                child: Text(
+                                  "4.9",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: fntfamily),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(right: 14, left: 14),
+              child: ListTile(
+                leading: Positioned(
+                  top: 1,
+                  child: Container(
+                    height: 60,
+                    width: screenWidth * 0.26 / 2,
+                    decoration: BoxDecoration(
+                      color: Colors.indigoAccent.shade100,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Icon(Icons.location_searching_outlined,
+                        color: Colors.white),
+                  ),
+                ),
+                title: Text(
+                  "Malik Travel,Spain",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: screenWidth / 22,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: fntfamily),
+                ),
+                subtitle: Row(
+                  children: [
+                    Icon(
+                      Icons.location_on_sharp,
+                      size: screenWidth / 24,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      " 44 Wal,Madrid",
+                      style: TextStyle(
+                          fontSize: screenWidth / 28,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: fntfamily),
+                    )
+                  ],
+                ),
+                trailing: Text(
+                  "\$450.0",
+                  style: TextStyle(
+                      fontSize: screenWidth / 26,
+                      color: Colors.tealAccent.shade400,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: fntfamily),
+                ),
+                dense: true,
+                isThreeLine: true,
+              ),
             ),
+            SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.only(left: 24, right: 24),
+              child: Text(
+                "The Similan Islands is and archipelago in the Andaman Sea off the coast of, and part of, Phang Nga Province southern Thiland. it is the maritime border between India and Thailand.it was established as Mu Ko Similan National Park in 1982 after a one-year assessment by the forestry department.",
+                style: TextStyle(
+                    fontSize: screenWidth / 28,
+                    fontFamily: fntfamily,
+                    color: Colors.black),
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(left: 24),
+                    child: imgprovider(
+                      img: "assets/img5.jpg",
+                    )),
+                imgprovider(img: "assets/img6.png"),
+                imgprovider(img: "assets/img7.jpg"),
+                Padding(
+                    padding: EdgeInsets.only(right: 24),
+                    child: imgprovider(img: "assets/img8.png")),
+              ],
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    child: Text(
+                      "Buy Now",
+                      style: TextStyle(
+                          fontSize:
+                              MediaQuery.of(context).size.width * 0.14 / 3,
+                          fontFamily: fntfamily,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shadowColor: MaterialStateProperty.all(Colors.black),
+                      elevation: MaterialStateProperty.all(2),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.tealAccent.shade400),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      minimumSize: MaterialStateProperty.all(
+                        Size(screenWidth, screenHeight),
+                      ),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16))),
+                    ),
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Container(
+                    height: screenHeight * 0.07,
+                    width: screenWidth * 0.66 / 4,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.tealAccent.shade400,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.tealAccent.shade400,
+                      size: 32,
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
